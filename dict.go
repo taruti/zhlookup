d3cc06f1d5db92bf9201df3ba2b84592d9a9cf02
Taru Karttunen <taruti@taruti.net>
2016-09-24 16:12:19 +0300

    Import code

---
+++ dict.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+var home = os.Getenv(`HOME`)
+var cedictFilename = home + `/.zhlookup.dict`
+
+func Find(s string) ([]string, error) {
+	sbs := []byte(s)
+
+	f, err := os.Open(cedictFilename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	var res []string
+	isExact := false
+	for scanner.Scan() {
+		bs := scanner.Bytes()
+		if len(bs) == 0 || bs[0] == '#' {
+			continue
+		}
+		var b1, b2 int
+		b1 = bytes.IndexByte(bs, ' ')
+		if b1 >= 0 {
+			b2 = bytes.IndexAny(bs[b1+1:], " [/")
+		}
+		if b2 <= 0 {
+			fmt.Printf("Non-standard entry: %q\n", bs)
+			continue
+		}
+		matchIdx := bytes.Index(bs, sbs)
+		if matchIdx < 0 {
+			continue
+		}
+		if (matchIdx == 0 || bs[matchIdx-1] == ' ') && (matchIdx+len(sbs) == len(bs) || bs[matchIdx+len(sbs)] == ' ') {
+			if !isExact {
+				res = nil
+				isExact = true
+			}
+			res = append(res, string(bs))
+		} else if !isExact {
+			res = append(res, string(bs))
+		}
+	}
+	return res, scanner.Err()
+}
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/chzyer/readline"
+)
+
+func main() {
+	flag.Parse()
+	err := mainWork()
+	if err != nil {
+		log.Fatal("ERROR", err)
+	}
+}
+
+func mainWork() error {
+	args := flag.Args()
+	for _, s := range args {
+		err := findPrint(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(args) == 0 {
+		err := console()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func findPrint(s string) error {
+	r, err := Find(s)
+	if err != nil {
+		return err
+	}
+	for _, line := range r {
+		fmt.Println(line)
+	}
+	return nil
+}
+
+func console() error {
+	rl, err := readline.New("> ")
+	if err != nil {
+		return err
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		err = findPrint(line)
+		if err != nil { // io.EOF
+			return err
+		}
+	}
+}
